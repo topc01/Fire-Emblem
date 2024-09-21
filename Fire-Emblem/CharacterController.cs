f00de0f39cbedd4dee8f02689a19e894f04146bf
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-21 14:56:41 -0300

    access data directly from stats

--- Fire-Emblem/CharacterController.cs
+++ Fire-Emblem/CharacterController.cs
@@ -2,10 +2,11 @@ namespace Fire_Emblem;
 
 public class CharacterController
 {
-    private readonly CharacterStats _character;
-    public CharacterController(CharacterStats character)
+    private CharacterStats? _character;
+    public CharacterStats Character
     {
-        _character = character;
+        set => _character = value;
+        get => _character ?? throw new InvalidOperationException("Character is not initialized.");
     }
     public string Attack(CharacterController defender)
     {
@@ -15,21 +16,21 @@ public class CharacterController
     }
     private int DamageAgainst(CharacterController opponent)
     {
-        int atk = _character.Atk;
-        double advantage = Armament.GetAdvantage(opponent.Armament);
-        double defense = Armament.IsMagic() ? opponent.Res : opponent.Def;
+        int atk = Character.Atk;
+        Armament armament = Character.Armament;
+        double advantage = armament.GetAdvantage(opponent.Character.Armament);
+        double defense = armament.IsMagic() ? opponent.Character.Res : opponent.Character.Def;
         return int.Max((int)(atk * advantage - defense), 0);
     }
-    private Armament Armament => _character.Armament;
-    private int Res => _character.Res;
-    private int Def => _character.Def;
-    private int Spd => _character.Spd;
-    private int Health => _character.Health;
-    private string WeaponName => _character.Weapon;
-    private string Name => _character.Name;
-    public bool IsAlive => _character.Health > 0;
-    public bool CanFollowUp(CharacterController opponent) => Spd - opponent.Spd >= 5;
-    private void ReceiveDamage(int damage) => _character.Health -= damage;
+    //private int Res => _character.Res;
+    //private int Def => _character.Def;
+    //private int Spd => _character.Spd;
+    //private int Health => _character.Health;
+    private string WeaponName => Character.Weapon;
+    private string Name => Character.Name;
+    public bool IsAlive() => Character.Health > 0;
+    public bool CanFollowUp(CharacterController opponent) => Character.Spd - opponent.Character.Spd >= 5;
+    private void ReceiveDamage(int damage) => Character.Health -= damage;
     public override bool Equals(object? obj)
     {
         if (obj == null || GetType() != obj.GetType())
@@ -42,15 +43,16 @@ public class CharacterController
     {
         foreach (var skill in skills)
         {
-            _character.Skills.Add(skill);
+            Character.Skills.Add(skill);
         }
     }
-    private bool HasRepeatedSkills() => _character.Skills.Count != _character.Skills.Distinct().Count();
-    private bool HasMoreThan2Skills() => _character.Skills.Count > 2;
+    private bool HasRepeatedSkills() => Character.Skills.Count != Character.Skills.Distinct().Count();
+    private bool HasMoreThan2Skills() => Character.Skills.Count > 2;
     public bool IsValidCharacter() => !HasRepeatedSkills() && !HasMoreThan2Skills();
     public string CheckAdvantages(CharacterController opponent)
     {
-        double advantage = Armament.GetAdvantage(opponent.Armament);
+        Armament armament = Character.Armament;
+        double advantage = armament.GetAdvantage(opponent.Character.Armament);
         return advantage switch
         {
             1.0 => "Ninguna unidad tiene ventaja con respecto a la otra",
@@ -59,5 +61,5 @@ public class CharacterController
             _ => throw new Exception($"Unknown advantage for {WeaponName} and {opponent.WeaponName}")
         };
     }
-    public override string ToString() => $"{Name} ({Health})";
+    public override string ToString() => $"{Name} ({Character.Health})";
 }
