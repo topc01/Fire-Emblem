37f03c3da7df67df7e608932f525e1a00160a401
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-21 14:23:35 -0300

    armament name

--- Fire-Emblem/CharacterStats.cs
+++ Fire-Emblem/CharacterStats.cs
@@ -5,14 +5,9 @@ public class CharacterStats : ICloneable
     public required string Name { get; set; }
     public required string Weapon
     {
-        init
-        {
-            Armament = Armament.GetArmamentFromName(value);
-            WeaponName = value;
-        } 
+        init => Armament = Armament.GetArmamentFromName(value);
+        get => Armament.Name;
     }
-
-    public readonly string WeaponName;
     public required string Gender { get; set; }
     public required string DeathQuote { get; set; }
     public required string HP
@@ -27,13 +22,14 @@ public class CharacterStats : ICloneable
     public required int Spd { get; set; }
     public required int Def { get; set; }
     public required int Res { get; set; }
-    public readonly List<Skill> Skills = new List<Skill>();
+    
+    public readonly List<Skill> Skills = new ();
     public readonly Armament Armament;
     private readonly int _maxHp;
     public int Health
     {
-        get => _health;
         set => _health = int.Max(0, int.Min(_maxHp, value));
+        get => _health;
     }
     private int _health;
     public object Clone() => this.MemberwiseClone();
