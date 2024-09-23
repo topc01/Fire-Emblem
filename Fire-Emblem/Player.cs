ade7cebfb444f453b4150557cc5f52533b569abe
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-23 00:34:10 -0300

    minor changes

--- Fire-Emblem/Player.cs
+++ Fire-Emblem/Player.cs
@@ -9,23 +9,18 @@ public class Player
     private readonly List<CharacterAnalyzer> _characters = new List<CharacterAnalyzer>();
     public Player(int playerNumber) => PlayerNumber = playerNumber;
     public bool HasLost() => !_characters.Any(character => character.IsAlive());
-    public int Length => _characters.Count;
     public void AddCharacter(CharacterAnalyzer characterAnalyzer) => _characters.Add(characterAnalyzer);
     public bool IsValidTeam() => IsValidLength() && AllValidCharacters() && !HasRepeatedCharacters();
     private bool IsValidLength() => _characters.Count is >= 1 and <= 3;
     private bool AllValidCharacters() => _characters.All(chr => chr.IsValidCharacter());
     private bool HasRepeatedCharacters() => _characters.Count != _characters.Distinct().Count();
-    public CharacterAnalyzer GetCharacter(int selected) => _characters[selected]
-                                                   ?? throw new Exception("No se encontró en el equipo");
-    public CharacterAnalyzer[] GetLiveCharacters()
+    private CharacterAnalyzer[] GetLiveCharacters()
         => _characters.Where(character => character.IsAlive()).ToArray();
-    
     private void Print(CharacterAnalyzer[] characters, View view) 
         => characters
         .Select((character, i) => $"{i}: {character.Name}")
         .ToList()
         .ForEach(view.WriteLine);
-
     private void Choose(CharacterAnalyzer[] characters, View view)
     {
         int choice;
@@ -33,7 +28,7 @@ public class Player
                || choice < 0 
                || choice >= characters.Length
                || !characters[choice].IsAlive()) ;
-        Controller.Character = characters[choice].Stats;
+        Controller.Character = characters[choice].Character;
     }
     public void SelectValidCharacter(View view)
     {
@@ -42,7 +37,6 @@ public class Player
         Print(liveCharacters, view);
         Choose(liveCharacters, view);
     }
-
     public override string ToString() => $"{Controller.Name} (Player {PlayerNumber}) comienza";
     public string CharacterFinalStatus => $"{Controller}";
     public string AdvantageMessage(Player opponent) => Controller.CheckAdvantages(opponent.Controller);
