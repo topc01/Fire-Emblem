3821c824637dd24ac2a589109ad721eba70b36b7
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 00:13:19 -0300

    condition abstract class receives the two character controllers

--- Fire-Emblem/Conditions/Condition.cs
+++ Fire-Emblem/Conditions/Condition.cs
@@ -1,7 +1,8 @@
+using Fire_Emblem.Characters;
 
 namespace Fire_Emblem.Conditions;
 
 public abstract class Condition
 {
-    public abstract bool DoesHold(CombatSummary combatSummary);
+    public abstract bool DoesHold(CharacterController character, CharacterController rival);
 }
