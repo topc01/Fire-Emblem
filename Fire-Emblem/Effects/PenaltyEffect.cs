c111296a9ede5e2e5291ae842f3fe70c6ecff708
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 16:36:13 -0300

    constructor in base class

--- Fire-Emblem/Effects/PenaltyEffect.cs
+++ Fire-Emblem/Effects/PenaltyEffect.cs
@@ -4,6 +4,7 @@ namespace Fire_Emblem.Effects;
 
 public abstract class PenaltyEffect : StatModifierEffect
 {
+    public PenaltyEffect(StatType targetStat, int bonus) : base(targetStat, bonus){}
     public override void Apply(CharacterController character)
         => Apply(character.Penalty);
     public abstract void Apply(AttackOrientedModifiedStats modifiedStats);
