f5fcea2c6f7be033e38dcef5040925339426c987
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 16:52:11 -0300

    penaltyEffect

--- Fire-Emblem/Effects/PenaltyEffect.cs
+++ Fire-Emblem/Effects/PenaltyEffect.cs
@@ -6,6 +6,21 @@ public abstract class PenaltyEffect : StatModifierEffect
 {
     public PenaltyEffect(StatType targetStat, int bonus) : base(targetStat, bonus){}
     public override void Apply(CharacterController character)
-        => Apply(character.Penalty);
-    public abstract void Apply(AttackOrientedModifiedStats modifiedStats);
+    {
+        switch (TargetStat)
+        {
+            case StatType.Atk:
+                character.Bonus.Atk = -Bonus;
+                break;
+            case StatType.Def:
+                character.Bonus.Def = -Bonus;
+                break;
+            case StatType.Res:
+                character.Bonus.Res = -Bonus;
+                break;
+            case StatType.Spd:
+                character.Bonus.Spd = -Bonus;
+                break;
+        }
+    }
 }
