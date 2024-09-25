bf9d6c17cb7b84d812c5c6064e7b698190f7d337
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 00:29:48 -0300

    Rival class receives a single condition

--- Fire-Emblem/Conditions/Rival.cs
+++ Fire-Emblem/Conditions/Rival.cs
@@ -1,12 +1,12 @@
 using Fire_Emblem.Characters;
 
-namespace Fire_Emblem.Conditions.CharacterConditions;
+namespace Fire_Emblem.Conditions;
 
 public class Rival : Condition
 {
-    private readonly BaseCondition _condition;
+    private readonly SingleCondition _condition;
     
-    public Rival(BaseCondition condition)
+    public Rival(SingleCondition condition)
         => _condition = condition;
     public override bool DoesHold(CharacterController character, CharacterController rival)
         => _condition.DoesHold(rival);
