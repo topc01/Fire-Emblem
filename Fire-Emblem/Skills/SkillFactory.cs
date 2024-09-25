fc8ce1cfc0721dc639fbfa99dd32f987c2c98f13
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 13:22:42 -0300

    more skills and shortcut for stat types

--- Fire-Emblem/Skills/SkillFactory.cs
+++ Fire-Emblem/Skills/SkillFactory.cs
@@ -1,3 +1,5 @@
+using Fire_Emblem.Conditions;
+using Fire_Emblem.Conditions.CharacterConditions;
 using Fire_Emblem.Conditions.LogicalConditions;
 using Fire_Emblem.Effects;
 
@@ -14,6 +16,11 @@ public class SkillFactory
         return createdSkill;
     }
 
+    private readonly StatType Atk = StatType.Atk;
+    private readonly StatType Def = StatType.Def;
+    private readonly StatType Res = StatType.Res;
+    private readonly StatType Spd = StatType.Spd;
+
     private Skill? CreateSkill(string name)
     {
         return name switch
@@ -24,23 +31,103 @@ public class SkillFactory
             "Single-Minded" => null,
             "Ignis" => null,
             "Perceptive" => null,
-            "Tome Precision" => null,
-            "Attack +6" => new Skill(
-                new BonusEffect(StatType.Atk, 6)),
-            "Speed +5" => new Skill(
-                new BonusEffect(StatType.Spd, 5)),
-            "Defense +5" => new Skill(
-                new BonusEffect(StatType.Def, 5)),
+            "Tome Precision"
+                => new Skill(
+                    new WeaponCondition(Armament.Magic),
+                    new MultiEffect(
+                        new BonusEffect(Atk, 6), new BonusEffect(Spd, 6))),
+            "Attack +6" 
+                => new Skill(
+                    new BonusEffect(Atk, 6)),
+            "Speed +5" 
+                => new Skill(
+                new BonusEffect(Spd, 5)),
+            "Defense +5" 
+                => new Skill(
+                new BonusEffect(Def, 5)),
             "Wrath" => null,
             "Resolve" => null,
-            "Resistance +5" => null,
-            "Atk/Def +5" => null,
-            "Atk/Res +5" => null,
-            "Spd/Res +5" => null,
-            "Deadly Blade" => null,
-            "Death Blow" => null,
-            "Armored Blow" => null,
-            "Darting Blow" => null,
+            "Resistance +5"
+                => new Skill(
+                    new BonusEffect(Res, 5)),
+            "Atk/Def +5"
+                => new Skill(
+                    new MultiEffect(
+                        new BonusEffect(Atk, 5), new BonusEffect(Def, 5))),
+            "Atk/Res +5"
+                => new Skill(
+                    new MultiEffect(
+                        new BonusEffect(Atk, 5), new BonusEffect(Res, 5))),
+            "Spd/Res +5"
+                => new Skill(
+                    new MultiEffect(
+                        new BonusEffect(Spd, 5), new BonusEffect(Res, 5))),
+            "Deadly Blade"
+                => new Skill(
+                    new AndCondition(
+                        new IsAttacker(), new WeaponCondition(Armament.Sword)),
+                    new MultiEffect(
+                        new BonusEffect(Atk, 8), new BonusEffect(Spd, 8))),
+            "Death Blow"
+                => new Skill(
+                new IsAttacker(),
+                        new BonusEffect(Atk, 8)),
+            "Armored Blow"
+                => new Skill(
+                    new IsAttacker(),
+                            new BonusEffect(Def, 8)),
+            "Darting Blow"
+                => new Skill(
+                    new IsAttacker(),
+                    new BonusEffect(Spd, 8)),
+            "Warding Blow"
+                => new Skill(
+                    new IsAttacker(),
+                    new BonusEffect(Res, 8)),
+            "Swift Sparrow"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Atk, 6), new BonusEffect(Spd, 6))),
+            "Sturdy Blow"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Atk, 6), new BonusEffect(Def, 6))),
+            "Mirror Strike"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Atk, 6), new BonusEffect(Res, 6))),
+            "Steady Blow"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Spd, 6), new BonusEffect(Def, 6))),
+            "Swift Strike"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Spd, 6), new BonusEffect(Res, 6))),
+            "Bracing Blow"
+                => new Skill(
+                    new IsAttacker(),
+                    new MultiEffect(
+                        new BonusEffect(Res, 6), new BonusEffect(Def, 6))),
+            "..." => null,
+            "Chaos Style"
+                => new Skill(
+                    new OrCondition(
+                        new AndCondition(
+                            new NotCondition(new WeaponCondition(Armament.Magic)),
+                            new Rival(new WeaponCondition(Armament.Magic))
+                            ),
+                        new AndCondition(
+                            (new WeaponCondition(Armament.Magic)),
+                            new NotCondition(new Rival(new WeaponCondition(Armament.Magic)))
+                        )
+                        ),
+                    new BonusEffect(Spd, 3)),
             _ => null
         };
         
