6f606231df2b8dd84f5b3b8b4394fbd2d8882ec8
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 00:18:23 -0300

    less condition

---
+++ Fire-Emblem/Conditions/CharacterConditions/LessCondition.cs
@@ -0,0 +1,18 @@
+using Fire_Emblem.Characters;
+
+namespace Fire_Emblem.Conditions.CharacterConditions;
+
+public class LessCondition : BaseCondition
+{
+    private readonly StatType _stat;
+    private readonly int _threshold;
+
+    public LessCondition(StatType target, int threshold)
+    {
+        _stat = target;
+        _threshold = threshold;
+    }
+    public override bool DoesHold(CharacterController character)
+        => character.GetStat(_stat) < _threshold;
+
+}
