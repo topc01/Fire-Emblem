929018e934107fed257ce31670e5b0dc310563fb
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 13:21:41 -0300

    set is attacker bool on round change

--- Fire-Emblem/Combat.cs
+++ Fire-Emblem/Combat.cs
@@ -63,8 +63,11 @@ public class Combat
     {
         _round++;
         (_attackingPlayer, _defendingPlayer) = (_defendingPlayer, _attackingPlayer);
+        _attackingPlayer.Controller.IsAttacker = true;
+        _defendingPlayer.Controller.IsAttacker = false;
     }
 
     public Player Winner()
         => _attackingPlayer.HasLost() ? _defendingPlayer : _attackingPlayer;
+    
 }
