04bdf680e0d3258a3b341c9a44e6abb601744033
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 20:30:22 -0300

    round method in Condition (unused)

--- Fire-Emblem/Conditions/Condition.cs
+++ Fire-Emblem/Conditions/Condition.cs
@@ -5,4 +5,6 @@ namespace Fire_Emblem.Conditions;
 public abstract class Condition
 {
     public abstract bool DoesHold(CharacterController character, CharacterController rival);
+    protected int Round(double value)
+        => Convert.ToInt32(Math.Floor(value));
 }
