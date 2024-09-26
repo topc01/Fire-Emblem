85fb82bcd9c43d01a495eb811a2c5c611ee52ff3
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-25 23:18:55 -0300

    set value to bonus or penalty depending on sign

--- Fire-Emblem/Effects/Effect.cs
+++ Fire-Emblem/Effects/Effect.cs
@@ -5,7 +5,8 @@ namespace Fire_Emblem.Effects;
 public abstract class Effect
 {
     public abstract void Apply(CharacterController character, CharacterController rival);
-
     protected int Round(double value)
         => Convert.ToInt32(Math.Floor(value));
+    protected AttackOrientedModifiedStats BonusOrPenalty(CharacterController character, int value)
+        => value > 0 ? character.Bonus : character.Penalty;
 }
