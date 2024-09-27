48fa0f9ca902c0d3e0448a14774091c4542d357e
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-09-27 11:49:28 -0300

    character attributes format

--- Fire-Emblem/Characters/CharacterStats.cs
+++ Fire-Emblem/Characters/CharacterStats.cs
@@ -2,14 +2,23 @@ namespace Fire_Emblem.Characters;
 
 public class CharacterStats : ICloneable
 {
-    public required string Name { get; set; }
+    public required string Name
+    {
+        get; set;
+    }
     public required string Weapon
     {
         init => Armament = Armament.GetArmamentFromName(value);
         get => Armament.Name;
     }
-    public required string Gender { get; set; }
-    public required string DeathQuote { get; set; }
+    public required string Gender
+    {
+        get; set;
+    }
+    public required string DeathQuote
+    {
+        get; set;
+    }
     public required string HP
     {
         init
@@ -18,34 +27,34 @@ public class CharacterStats : ICloneable
             _health = MaxHp;
         }
     }
-    public required int Atk { get; set; }
-    public required int Spd { get; set; }
-    public required int Def { get; set; }
-    public required int Res { get; set; }
-    
+    public required int Atk
+    {
+        get; set;
+    }
+    public required int Spd
+    {
+        get; set;
+    }
+    public required int Def
+    {
+        get; set;
+    }
+    public required int Res
+    {
+        get; set;
+    }
     public readonly Armament Armament = null!;
-    public int MaxHp { get; set; }
+    public int MaxHp
+    {
+        get; set;
+    }
     public int Health
     {
         set => _health = int.Max(0, int.Min(MaxHp, value));
         get => _health;
     }
     private int _health;
-
-    public CharacterStats LastRival;
-    // hasta acá es una EDD
-    
-    //public bool IsAlive() => Health > 0;
-    //public void AddSkills(Skill[] skills)
-    //{
-    //    foreach (var skill in skills)
-    //    {
-    //        Skills.Add(skill);
-    //    }
-    //}
-    //public bool IsValidCharacter() => !HasRepeatedSkills() && !HasMoreThan2Skills();
-    //private bool HasRepeatedSkills() => Skills.Count != Skills.Distinct().Count();
-    //private bool HasMoreThan2Skills() => Skills.Count > 2;
+    public CharacterStats? LastRival;
     public object Clone() => this.MemberwiseClone();
     public CharacterStats New() => (CharacterStats)Clone();
 }
