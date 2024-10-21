fd623feb7e8f98a5194afe645059bb55d77702eb
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-10-21 12:15:04 -0300

    typo

--- Fire-Emblem/Effects/EffectImplementations/SandstormEffect.cs
+++ Fire-Emblem/Effects/EffectImplementations/SandstormEffect.cs
@@ -7,7 +7,7 @@ public class SandstormEffect : CharacterEffect
     public override void Apply(CharacterController controller)
     {
         double defenseWeightedBy150 = controller.Character.Def * 1.5;
-        int differenceWithWeightededDefense = Round(defenseWeightedBy150 - controller.Character.Atk);
-        ApplyToBonusOrPenalty(controller, differenceWithWeightededDefense).Atk.FollowUp += differenceWithWeightededDefense;
+        int differenceWithWeightedDefense = Round(defenseWeightedBy150 - controller.Character.Atk);
+        ApplyToBonusOrPenalty(controller, differenceWithWeightedDefense).FollowUp.Atk += differenceWithWeightedDefense;
     }
 }
