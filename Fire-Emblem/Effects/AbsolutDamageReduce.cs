c4b3907ad424db9d19620ce1bfc20aac9a2335ed
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-17 13:02:23 -0300

    using value as argument name

--- Fire-Emblem/Effects/AbsolutDamageReduce.cs
+++ Fire-Emblem/Effects/AbsolutDamageReduce.cs
@@ -2,10 +2,10 @@ using Fire_Emblem.Characters;
 
 namespace Fire_Emblem.Effects;
 
-public class AbsolutDamageReduce(int quantity) : CharacterEffect
+public class AbsolutDamageReduce(int value) : CharacterEffect
 {
     public override void Apply(CharacterController controller)
     {
-        controller.Combat.AbsoluteDamageReduction = quantity;
+        controller.Combat.AbsoluteDamageReduction = value;
     }
 }
--- Fire-Emblem/Effects/ExtraDamage.cs
+++ Fire-Emblem/Effects/ExtraDamage.cs
@@ -3,27 +3,15 @@ using Fire_Emblem.Types;
 
 namespace Fire_Emblem.Effects;
 
-public class ExtraDamage : CharacterEffect
+public class ExtraDamage(int value, BattleStage stage = BattleStage.Combat) : CharacterEffect
 {
-    private readonly int _quantity;
-    private readonly BattleStage _stage;
-    public ExtraDamage(int quantity)
-    {
-        _quantity = quantity;
-        _stage = BattleStage.Combat;
-    }
-    public ExtraDamage(BattleStage stage, int quantity)
-    {
-        _stage = stage;
-        _quantity = quantity;
-    }
     public override void Apply(CharacterController controller)
     {
-        if (_stage == BattleStage.Combat)
-            controller.Combat.ExtraDamage += _quantity;
-        else if (_stage == BattleStage.FirstAttack)
-            controller.FirstAttack.ExtraDamage += _quantity;
+        if (stage == BattleStage.Combat)
+            controller.Combat.ExtraDamage += value;
+        else if (stage == BattleStage.FirstAttack)
+            controller.FirstAttack.ExtraDamage += value;
         else
-            controller.FollowUp.ExtraDamage += _quantity;
+            controller.FollowUp.ExtraDamage += value;
     }
 }
