037034f533c45a1b664830bd354b5ed9e378a71c
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-17 19:53:45 -0300

    allow comparing between different stats

--- Fire-Emblem/Conditions/CharacterConditions/StatGreaterThanRival.cs
+++ Fire-Emblem/Conditions/CharacterConditions/StatGreaterThanRival.cs
@@ -3,8 +3,21 @@ using Fire_Emblem.Types;
 
 namespace Fire_Emblem.Conditions.CharacterConditions;
 
-public class StatGreaterThanRival(StatType stat) : Condition
+public class StatGreaterThanRival : Condition
 {
+    private readonly StatType _stat;
+    private readonly StatType _rivalStat;
+
+    public StatGreaterThanRival(StatType stat)
+    {
+        _stat = stat;
+        _rivalStat = stat;
+    }
+    public StatGreaterThanRival(StatType stat, StatType rivalStat)
+    {
+        _stat = stat;
+        _rivalStat = rivalStat;
+    }
     public override bool DoesHold(CharacterController character, CharacterController rival)
-        => character.GetStatWithoutSpecificModificators(stat) > rival.GetStatWithoutSpecificModificators(stat);
+        => character.GetStatWithoutSpecificModificators(_stat) > rival.GetStatWithoutSpecificModificators(_rivalStat);
 }
