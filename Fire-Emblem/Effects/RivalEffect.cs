921892a5d26b7dd50185fb6f9dea017ce3190830
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-19 15:23:26 -0300

    cleaner constructor

--- Fire-Emblem/Effects/RivalEffect.cs
+++ Fire-Emblem/Effects/RivalEffect.cs
@@ -4,11 +4,7 @@ namespace Fire_Emblem.Effects;
 
 public class RivalEffect(CharacterEffect effect) : BaseEffect
 {
-    private readonly CharacterEffect _effect;
-
-    public RivalEffect(CharacterEffect effect)
-        => _effect = effect;
 
     public override void Apply(CharacterController character, CharacterController rival)
-        => _effect.Apply(rival);
+        => effect.Apply(rival);
 }
