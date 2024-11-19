dbcf49909e1323b63ed17680f85f6e10ad07e79f
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-19 20:00:01 -0300

    virtual CalculateReduction for easy implementation

--- Fire-Emblem/Effects/CommonEffects/PercentageDamageReduce.cs
+++ Fire-Emblem/Effects/CommonEffects/PercentageDamageReduce.cs
@@ -3,21 +3,21 @@ using Fire_Emblem.Types;
 
 namespace Fire_Emblem.Effects.CommonEffects;
 
-public class PercentageDamageReduce(int reduction, BattleStage stage = BattleStage.Combat, EffectType type = EffectType.PercentageReduction) : CharacterEffect
+public class PercentageDamageReduce(int value, BattleStage stage = BattleStage.Combat, EffectType type = EffectType.PercentageReduction) : BaseEffect
 {
     protected override EffectType Type { get; set; } = type;
 
-    public override void Apply(CharacterController controller)
+    public override void Apply(CharacterController controller, CharacterController rival)
     {
+        int reduction = CalculateReduction(controller, rival);
         if (stage == BattleStage.Combat)
             controller.Combat.PercentageDamageReduction = reduction;
         else if (stage == BattleStage.FirstAttack)
             controller.FirstAttack.PercentageDamageReduction = reduction;
         else controller.FollowUp.PercentageDamageReduction = reduction;
     }
-
-    /*private void Func()
-    {
-        if (Type == EffectType.Callback) ;
-    }*/
+    
+    protected virtual int CalculateReduction(CharacterController controller, CharacterController rival)
+        => value;
+    
 }
--- Fire-Emblem/Effects/CommonEffects/PercentageDamageReduceByStatDifference.cs
+++ Fire-Emblem/Effects/CommonEffects/PercentageDamageReduceByStatDifference.cs
@@ -3,14 +3,13 @@ using Fire_Emblem.Types;
 
 namespace Fire_Emblem.Effects.CommonEffects;
 
-public class PercentageDamageReduceByStatDifference(StatType stat, int factor = 4, int max = int.MaxValue) : BaseEffect
+public class PercentageDamageReduceByStatDifference(StatType stat, int factor = 4, int max = int.MaxValue, BattleStage stage = BattleStage.Combat, EffectType type = EffectType.PercentageReduction) : PercentageDamageReduce(0, stage, type)
 {
-    public override void Apply(CharacterController controller, CharacterController rival)
+    protected override int CalculateReduction(CharacterController controller, CharacterController rival)
     {
         int controllerStat = controller.GetStatWithoutSpecificModificators(stat);
         int rivalStat = rival.GetStatWithoutSpecificModificators(stat);
         int difference = controllerStat - rivalStat;
-        int reduction = int.Min(difference * factor, max);
-        controller.Combat.PercentageDamageReduction = reduction;
+        return int.Min(difference * factor, max);
     }
 }
