abca8d35bec93065b9254132d42a7258533c1735
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-19 15:19:54 -0300

    Description in base skill

--- Fire-Emblem/Skills/BaseSkill.cs
+++ Fire-Emblem/Skills/BaseSkill.cs
@@ -5,8 +5,9 @@ namespace Fire_Emblem.Skills;
 public abstract class BaseSkill
 {
     public string Name = "";
+    public string Description { get; set; } = null!;
     public abstract void ApplyIfDoesHold(CharacterController controller, CharacterController rival);
-    
+
     public override bool Equals(object? obj)
     {
         if (obj == null || GetType() != obj.GetType())
