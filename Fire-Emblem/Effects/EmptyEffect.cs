42d43d7250be82995cce3923af049fa23a176437
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-19 15:20:21 -0300

    new empty effect

---
+++ Fire-Emblem/Effects/EmptyEffect.cs
@@ -0,0 +1,10 @@
+using Fire_Emblem.Characters;
+
+namespace Fire_Emblem.Effects;
+
+public class EmptyEffect : BaseEffect
+{
+    public override void Apply(CharacterController controller, CharacterController rival)
+    {
+    }
+}
