ad4912fbfb2a4ab005de3797878e126c2380787f
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-12-01 01:54:28 -0300

    add a difference option to compare stats

--- Fire-Emblem/Conditions/CharacterConditions/StatGreaterThanRival.cs
+++ Fire-Emblem/Conditions/CharacterConditions/StatGreaterThanRival.cs
@@ -7,17 +7,20 @@ public class StatGreaterThanRival : Condition
 {
     private readonly StatType _stat;
     private readonly StatType _rivalStat;
+    private readonly int _difference;
 
-    public StatGreaterThanRival(StatType stat)
+    public StatGreaterThanRival(StatType stat, int diff = 0)
     {
         _stat = stat;
         _rivalStat = stat;
+        _difference = diff;
     }
-    public StatGreaterThanRival(StatType stat, StatType rivalStat)
+    public StatGreaterThanRival(StatType stat, StatType rivalStat, int diff = 0)
     {
         _stat = stat;
         _rivalStat = rivalStat;
+        _difference = diff;
     }
     public override bool DoesHold(CharacterController character, CharacterController rival)
-        => character.GetStatWithoutSpecificModificators(_stat) > rival.GetStatWithoutSpecificModificators(_rivalStat);
+        => character.GetStatWithoutSpecificModificators(_stat) > rival.GetStatWithoutSpecificModificators(_rivalStat) + _difference;
 }
