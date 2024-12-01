85525bc9fb770d825578e726017507bdcf25d423
Tomás Pfingsthorn Cuevas <tpfingsthorn1@uc.cl>
2024-11-30 21:48:20 -0300

    Lunar Brace is set to Callback

--- Fire-Emblem/Effects/EffectImplementations/LunarBraceEffect.cs
+++ Fire-Emblem/Effects/EffectImplementations/LunarBraceEffect.cs
@@ -5,9 +5,15 @@ namespace Fire_Emblem.Effects.EffectImplementations;
 
 public class LunarBraceEffect : BaseEffect
 {
+    public LunarBraceEffect()
+    {
+        Type = EffectType.Callback;
+    }
+    
     public override void Apply(CharacterController controller, CharacterController rival)
     {
         int rivalDef = rival.GetStatWithoutSpecificModificators(StatType.Def);
-        controller.Combat.ExtraDamage += Truncate(rivalDef * 0.3);
+        int pondered = Truncate(rivalDef * 0.3);
+        controller.Combat.ExtraDamage += pondered;
     }
 }
